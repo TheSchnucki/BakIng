703eefb0aae7b5d0f35dfa649efc399e35878a16
theSchnucki <mark.schneider@gmx.net>
2018-06-18 11:41:12 +0200

    Recipe List is working again.

--- app/src/main/java/com/theschnucki/baking/chooseRecipe/RecipeActivity.java
+++ app/src/main/java/com/theschnucki/baking/chooseRecipe/RecipeActivity.java
@@ -15,6 +15,6 @@ public class RecipeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preperation);
+        setContentView(R.layout.activity_recipe);
     }
 }
